a192312f69d40f71630d6cb5f737913d4ab40fd4
Philip Rey Raquinel <devphil.work@gmail.com>
2023-08-19 23:52:11 +0800

    Addd Media Folder & Data

--- btrealestate/settings.py
+++ btrealestate/settings.py
@@ -128,6 +128,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btrealestate/static')
 ]
 
+
+# Media folder settings
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
