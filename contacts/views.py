786eacee48ceb0d312fc4943e4d1c71b54650b53
Philip Rey Raquinel <devphil.work@gmail.com>
2023-08-31 22:59:03 +0800

    Add Inquiry Check & Send Email

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -13,9 +14,26 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        # Check if user has made inquiry already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/' + listing_id)
+
         contact = Contact(listing_id=listing_id, listing=listing, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
 
-        messages.success(request, 'Inquiry sent, a realtor will get back to you soon')
+        # # Send email
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'There has been inquiry for ' + listing + '. Sign into the admin panel for more info',
+        #     '(host email here)',
+        #     [realtor_email, '(host email here)'],
+        #     fail_silently=False
+        # )
 
+        messages.success(request, 'Inquiry sent, a realtor will get back to you soon')
         return redirect('/listings/' + listing_id)
